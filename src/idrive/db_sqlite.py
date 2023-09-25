a5b7679fdec6e52df0e8cd1dd315a9e926c66f15
Eric Powers <eric@mrpowers.com>
2023-09-24 17:48:21 -0700

    Revert to include folder size as number of files.

--- src/idrive/db_sqlite.py
+++ src/idrive/db_sqlite.py
@@ -6,7 +6,7 @@ import socket
 
 
 APP_NAME = 'idrive-backup'
-DB_NAME = 'index.db'
+DEFAULT_DB_NAME = 'index.db'
 
 class FileStatus(enum.IntEnum):
     DEFAULT = -1
@@ -17,41 +17,51 @@ class FileStatus(enum.IntEnum):
 log = logging.getLogger(__name__.split('.',1)[0])
 
 
-########
-#
-# SQLite
-#
-########
-
 __hostname = socket.gethostname()
+
+def __get_host():
+    return __hostname
+
 
 __cache_dir = None
 
-def __get_cache_dir():
+def __get_cache_dir(create=False):
     global __cache_dir
     if __cache_dir is None:
         cache_home = os.getenv('XDG_CACHE_HOME', None) or os.path.expanduser('~/.cache')
         cache_home = os.path.abspath(cache_home)
-        if not os.path.isdir(cache_home):
-            os.mkdir(cache_home)
         cache_dir = os.path.join(cache_home, APP_NAME)
-        if not os.path.isdir(cache_dir):
+        if os.path.isdir(cache_dir):
+            __cache_dir = cache_dir
+        elif create:
+            if not os.path.isdir(cache_home):
+                os.mkdir(cache_home)
             os.mkdir(cache_dir)
-        __cache_dir = cache_dir
+            __cache_dir = cache_dir
     return __cache_dir
 
 
 __db_name = None
 
-def db_init(db_name=None):
-    global __db_name
-    if not db_name:
-        db_name = DB_NAME
+def __get_db_name(db_name=None, host=None, device_id=None):
+    # if a db name was specified, always use that.
+    if __db_name:
+        return __db_name
 
-    cache_dir = __get_cache_dir()
-    db_path = os.path.join(cache_dir, db_name)
+    # if host and/or device, construct a db name.
+    if host or device_id:
+        return '.'.join(filter(None, (host, device_id))) + '.db'
+
+    # otherwise default
+    return DEFAULT_DB_NAME
+
+
+def __db_create(db_name, db_dir=None):
+    if not db_dir:
+        db_dir = __get_cache_dir(True)
+    db_path = os.path.join(db_dir, db_name)
     if not os.path.isfile(db_path):
-        #tmp_db_path = os.path.join(cache_dir, DB_NAME)
+        #tmp_db_path = os.path.join(db_dir, DEFAULT_DB_NAME)
         #if os.path.isfile(tmp_db_path):
         #    os.remove(tmp_db_path)
         #conn = SQL.connect(tmp_db_path)
@@ -62,25 +72,38 @@ def db_init(db_name=None):
         #    os.remove(tmp_db_path)
         #else:
         #    os.rename(tmp_db_path, db_path)
+
+
+def db_init(db_name=None, host=None, device_id=None):
+    # set the current db name
+    global __db_name
     __db_name = db_name
 
+    # get the default db name
+    db_name = __get_db_name(host=host, device_id=device_id)
 
-__db_conn = None
+    # create the default db
+    __db_create(db_name)
 
-def __db_get_conn():
-    global __db_conn
-    if __db_conn is None:
+
+__db_connections = dict()
+
+def __db_get_conn(host=None, device_id=None):
+    key = (str(host) if host is not None else None, str(device_id) if device_id is not None else None)
+    conn = __db_connections.get(key)
+    if conn is None:
         cache_dir = __get_cache_dir()
-        db_path = os.path.join(cache_dir, __db_name)
+        db_name = __get_db_name(host=host, device_id=device_id)
+        db_path = os.path.join(cache_dir, db_name)
         if not os.path.isfile(db_path):
             raise
         else:
             conn = SQL.connect(db_path)
-        __db_conn = conn
-    return __db_conn
+    return conn
 
-def __db_cursor():
-    conn = __db_get_conn()
+def __db_cursor(host=None, device_id=None):
+    host = host if host is not None else __hostname
+    conn = __db_get_conn(host=host, device_id=device_id)
     cursor = conn.cursor()
     return cursor
 
@@ -143,6 +166,7 @@ def __db_insert_file(cursor, folder, filename, host=None, device_id=None, st_inf
         host=host if host is not None else __hostname,
         folder=__folder_path(folder),
         filename=filename,
+        code=FileStatus.DEFAULT,
     )
     if device_id is not None:
         data.update(dict(
@@ -179,13 +203,14 @@ def __db_insert_folder(cursor, folder, host=None, device_id=None):
 
 def __db_any_file_path(path):
     '''Find any matching file by path and return bool.'''
+    host = __hostname
     fields = ('size',)
     where = dict(
-        host=__hostname,
+        host=host,
         folder=__folder_path(os.path.dirname(path)),
         filename=os.path.basename(path),
     )
-    cursor = __db_cursor()
+    cursor = __db_cursor(host=host)
     result = __db_cursor_select_fetchone_file(cursor, fields, where)
     return result is not None
 
@@ -202,16 +227,17 @@ def __db_get_folder_size(host, folder, device_id=None):
         where.update(dict(
             device_id=device_id,
         ))
-    cursor = __db_cursor()
+    cursor = __db_cursor(host=host, device_id=device_id)
     result = __db_cursor_select_fetchone_file(cursor, fields, where)
     return result
 
 
 def __db_fetch_next_folder(host=None, device_id=None):
     '''Find a matching folder and return path.'''
+    host = host if host is not None else __hostname
     fields = ('folder',)
     where = dict(
-        host=host if host is not None else __hostname,
+        host=host,
         filename="",
         code=FileStatus.DEFAULT,
     )
@@ -219,7 +245,7 @@ def __db_fetch_next_folder(host=None, device_id=None):
         where.update(dict(
             device_id=device_id,
         ))
-    cursor = __db_cursor()
+    cursor = __db_cursor(host=host, device_id=device_id)
     result = __db_cursor_select_fetchone_file(cursor, fields, where)
     return result
 
@@ -234,7 +260,7 @@ def __db_has_folder(folder, host=None, device_id=None):
 
 def db_insert_folder(folder, host=None, device_id=None):
     '''Add folder into database.'''
-    cursor = __db_cursor()
+    cursor = __db_cursor(host=host, device_id=device_id)
     __db_insert_folder(cursor, folder, host=host, device_id=device_id)
     cursor.connection.commit()
 
